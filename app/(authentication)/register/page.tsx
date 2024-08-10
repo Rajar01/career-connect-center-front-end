14aa254bc140c1de21020550d8944e3cdfa02254
Rajar01 <awangpa08@gmail.com>
2024-08-10 08:03:46 +0700

    feat: add register page ui

--- app/(authentication)/register/page.tsx
+++ app/(authentication)/register/page.tsx
@@ -6,14 +6,14 @@ import {
 } from '@nextui-org/react';
 import FeatherIcon from 'feather-icons-react';
 
-function LoginPage() {
+function RegisterPage() {
   const [isVisible, setIsVisible] = React.useState(false);
 
   return (
     <main className="h-screen flex flex-row items-center justify-between gap-x-[130px] pl-[130px] bg-white">
       <div className="basis-4/12 flex flex-col items-center justify-center text-darkAccent">
-        <h1 className="text-[40px] font-bold">Welcome back</h1>
-        <h2 className="text-[19px] mb-4">Log In to Discover New Opportunities</h2>
+        <h1 className="text-[40px] font-bold">Create Your Account</h1>
+        <h2 className="text-[19px] mb-4">Connect with talent and opportunities</h2>
         <Tabs
           className="mb-6"
           color="primary"
@@ -26,6 +26,23 @@ function LoginPage() {
           <Tab key="employer" title="Employer" />
           <Tab key="recruiter" title="Recruiter" />
         </Tabs>
+        <Input
+          classNames={{
+            base: 'h-[52px] mb-3',
+            inputWrapper: [
+              'rounded-lg h-full px-3',
+              'group-data-[focus=true]:border-primary group-data-[focus=true]:border-2',
+              'group-data-[hover=true]:border-primary',
+            ],
+            input: [
+              'text-base !text-darkAccent',
+              'placeholder:text-[#B3B3B3]',
+              'data-[has-start-content=true]:ps-0 data-[has-end-content=true]:pe-0',
+            ],
+          }}
+          variant="bordered"
+          placeholder="Full name"
+        />
         <Input
           classNames={{
             base: 'h-[52px] mb-3',
@@ -73,17 +90,51 @@ function LoginPage() {
                 />
               )}
             </button>
-          )}
+                  )}
           variant="bordered"
           placeholder="Password"
           type={isVisible ? 'text' : 'password'}
         />
-        <Link href="/" className="self-end mb-5 text-darkAccent">Forgot password?</Link>
-        <Button className="h-[52px] mb-2 rounded-lg text-base" fullWidth color="primary">Login</Button>
+        <Input
+          classNames={{
+            base: 'h-[52px]  mb-5',
+            inputWrapper: [
+              'rounded-lg h-full px-3',
+              'group-data-[focus=true]:border-primary group-data-[focus=true]:border-2',
+              'group-data-[hover=true]:border-primary',
+            ],
+            input: [
+              'text-base !text-darkAccent',
+              'placeholder:text-[#B3B3B3]',
+              'data-[has-start-content=true]:ps-0 data-[has-end-content=true]:pe-0',
+            ],
+          }}
+          endContent={(
+            <button type="button" onClick={() => setIsVisible(!isVisible)}>
+              {isVisible ? (
+                <FeatherIcon
+                  icon="eye"
+                  size={20}
+                  className="text-[#B3B3B3] group-data-[focus=true]:text-darkAccent group-data-[has-value=true]:text-darkAccent"
+                />
+              ) : (
+                <FeatherIcon
+                  icon="eye-off"
+                  size={20}
+                  className="text-[#B3B3B3] group-data-[focus=true]:text-darkAccent group-data-[has-value=true]:text-darkAccent"
+                />
+              )}
+            </button>
+                  )}
+          variant="bordered"
+          placeholder="Password confirmation"
+          type={isVisible ? 'text' : 'password'}
+        />
+        <Button className="h-[52px] mb-2 rounded-lg text-base" fullWidth color="primary">Register</Button>
         <span className="self-start">
-          Don’t have an account?
+          Have an account?
           {' '}
-          <Link href="/register" className="text-darkAccent underline underline-offset-2">Register</Link>
+          <Link href="/" className="text-darkAccent underline underline-offset-2">Log in</Link>
         </span>
       </div>
       <div className="h-full basis-8/12 py-4 pr-4">
@@ -107,4 +158,4 @@ function LoginPage() {
   );
 }
 
-export default LoginPage;
+export default RegisterPage;
